aaa9397405078e4436a3357c77d48f9128b4478c
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-27 23:20:24 -0400

    set up openai.js

---
+++ routes/openai.js
@@ -0,0 +1,18 @@
+import express from "express"
+import axios from "axios"
+import dotenv from  "dotenv"
+import { openai } from "../index.js"
+
+dotenv.config()
+const router = express.Router()
+
+
+router.post("/text", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body
+    res.status(200)
+  } catch (error) {
+    console.log("error", error)
+    res.status(500).json({ error: error.message }) 
+  }
+})
