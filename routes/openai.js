17a96e31f8be16a718eb03b72802d073a0981987
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-28 09:21:16 -0400

    pass in text to res.status

--- routes/openai.js
+++ routes/openai.js
@@ -10,7 +10,8 @@ const router = express.Router()
 router.post("/text", async (req, res) => {
   try {
     const { text, activeChatId } = req.body
-    res.status(200)
+    console.log("text", text)
+    res.status(200).json({ text })
   } catch (error) {
     console.log("error", error)
     res.status(500).json({ error: error.message }) 
